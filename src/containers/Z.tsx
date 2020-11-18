8884322cd1ef4f6f82aaef81e5285ca23b29d671
wangyulue <1206180097@qq.com>
2020-11-18 23:57:34 +0800

    feat(*):optimize code

--- src/containers/Z.tsx
+++ src/containers/Z.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { store } from '../store';
 
-function myAction(dispatch) {
-  dispatch({ type: 'INCREMENT' })
-  setTimeout(() => {
-    dispatch({ type: 'DECREMENT' })
-  }, 1000)
+function myAction(message: string) {
+  return (dispatch) => {
+    dispatch({ type: 'INCREMENT', message })
+    setTimeout(() => {
+      dispatch({ type: 'DECREMENT', message })
+    }, 1000)
+  }
 }
-
 
 export default function App() {
   const onClick = () => {
     // 使用 redux-chunk 中间件后，这种写法能生效
-    store.dispatch(myAction)
+    store.dispatch(myAction('loading'))
     // 如果没有使用 redux-chunk 中间件，可以使用以下的写法
-    // myAction(store.dispatch);
+    // myAction('loading')(store.dispatch);
   }
   return (<div>
     <button onClick={onClick}>测试</button>
